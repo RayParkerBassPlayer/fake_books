1f3fc960f3e3007d607a39efb2d1a9de86a10264
Ray Parker <RayParkerBassPlayer@gmail.com>
2019-04-21 17:43:20 -0400

    Fixed workflow edge cases, added error message.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,22 @@
 class HomeController < ApplicationController
   def home
-    @tunes = Tune.includes(:locations => :book).order("tunes.title").all
+    @tunes = []
+    @search_string = nil
   end
 
   def find
     @search_string = params[:search_string]
-    @tunes = Tune.includes(:locations => :book).where("tunes.title like '%#{@search_string}%'")
+
+    if @search_string.present?
+      @tunes = Tune.includes(:locations => :book).where("tunes.title like '%#{@search_string}%'")
+    else
+      @tunes = []
+      @message = "Please key in a search."
+    end
+
+    if @tunes.empty? && @message.blank?
+      @message = "'#{@search_string}' turned up no results.'"
+    end
 
     render :home
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post "/find", :to => "home#find", :as => :find_tune
+  get "/find", :to => "home#home", :as => :home_home
   root :to => "home#home"
 end
