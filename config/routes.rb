d945d46f02a30a594af9dccf591388f5f01d92f1
Ray Parker <RayParkerBassPlayer@gmail.com>
2019-04-22 12:09:16 -0400

    Added default bad-route redirection.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   post "/find", :to => "home#find", :as => :find_tune
   get "/find", :to => "home#home", :as => :home_home
   root :to => "home#home"
+  match '*path' => 'home#find', via: :all
 end
